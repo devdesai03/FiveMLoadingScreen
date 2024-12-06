d906af875443387496c4e6243b87e5317c27a277
Dev Desai <desaidev0110@gmail.com>
2024-12-05 18:49:29 -0600

    changes made

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,24 +1,39 @@
 shared_scripts { '@FiniAC/fini_events.lua' }
 
 
-
-
-
-fx_version 'adamant'
+fx_version 'cerulean'
 game 'gta5'
 
-author 'Joltz'
-description 'luxury-rp-loading-screen'
-version '0.1'
+author ''
+description 'Luxury RP Loading Screen'
+version '1.0.0'
 
 files {
-    '*.html',
-    'assets/**/*.*',
-    'assets/**/**/*.*'
+    'index.html',
+    'favicon.ico',
+    'assets/css/style.css',
+    'assets/css/fonts.css',
+    'assets/css/all.min.css',
+    'assets/scripts/main.js',
+    'assets/scripts/particles.json',
+    'assets/scripts/all.min.js',
+    'assets/scripts/config.js',
+    'assets/media/video/video_1.mp4',
+    'assets/media/logo.png',
+    'assets/media/border.png',
+    'assets/media/icons/cursor.png',
+    'assets/media/icons/chevron.png',
+    'assets/media/icons/discord.png',
+    'assets/media/icons/tebex.png',
+    'assets/media/cover/song1.png',
+    'assets/media/cover/song2.png',
+    'assets/media/cover/song3.png',
+    'assets/media/cover/song4.jpg',
+    'assets/media/songs/song_1.mp3',
+    'assets/media/songs/song_2.mp3',
+    'assets/media/songs/song_3.mp3',
+    'assets/media/songs/song_4.mp3',
+    -- Add all required files
 }
 
-
---loadscreen_manual_shutdown "yes"
 loadscreen 'index.html'
-
-
